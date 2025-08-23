80c7024f3d7b48f05d26aa49e689327d7ac9e180
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-23 18:30:39 +0545

    completed buildin movie app for android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -39,16 +39,15 @@ export default function Index() {
   );
 
   return (
-    <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg}
+      className="flex-1 absolute w-full z-0"
+      />
 
-      <ScrollView
+       <ScrollView
         className="flex-1 px-5"
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          minHeight: "100%",
-          paddingBottom: 10,
-        }}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
