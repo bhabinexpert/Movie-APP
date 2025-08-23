cc11788fe581b39b391df03e9d8fd88bbc0961f4
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-23 15:47:23 +0545

    set the trending movies according to user search and fetched those movies in the trending panel in home page with customization with MaskedView packaged

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,8 +5,8 @@ import { icons } from '@/constants/icons'
 interface Props{
     placeholder: string,
     onPress?: ()=> void;
-    value: string;
-    onChangeText: (text: string)=> void;
+    value?: string;
+    onChangeText?: (text: string)=> void;
 } // interface basically defines what the types of props are in tsx.
 
 const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
